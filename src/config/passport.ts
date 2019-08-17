fe76593bee460a0f8930d4866dfff1e62a02e7a3
Abdul Rehman <mehars.6925@gmail.com>
2019-08-18 04:02:51 +0500

    removed passport jwt

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,19 +1,6 @@
 import passport from "passport";
 import { User } from "../models";
 import { Strategy as LocalStrategy } from "passport-local";
-import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
-
-passport.use(new JwtStrategy({
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: process.env.SECRET_KEY || "sup3rs3cr3t"
-}, (jwtPayload, callback) => {
-    return User.findById(jwtPayload.id)
-        .select("-password")
-        .then(user => {
-            if(!user) return callback(null, false);
-            return callback(null, user);
-        }).catch(error => callback(error, false));
-}));
 
 passport.use(new LocalStrategy({
     usernameField: "email",
